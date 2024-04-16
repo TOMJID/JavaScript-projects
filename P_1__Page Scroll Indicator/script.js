2f5b6ebc5586207cf958da31c3e3277cbd4d2b49
TOMJID <94817887+TOMJID@users.noreply.github.com>
2024-04-16 16:00:38 +0600

    p-1__page scroll indicator complete

--- P_1__Page Scroll Indicator/script.js
+++ P_1__Page Scroll Indicator/script.js
@@ -1,7 +1,6 @@
 const indicator = document.querySelector('.scroll__indicator .progress');
 const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
 
-
 function scroll() {
   const { scrollTop } = document.documentElement;
   const scrolled = (scrollTop / scrollHeight) * 100;
